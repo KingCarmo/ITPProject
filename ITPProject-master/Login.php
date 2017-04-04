90bb2aab5e4b16532e48daa5ff5b73c26c842d83
PeterFinegan <peterfinegan25@hotmail.com>
2017-04-04 12:43:04 +0100

    Add files via upload

---
+++ ITPProject-master/Login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Cocktail Dreams</title>
+    <link type="text/css" rel="stylesheet" href="style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="container">
+      <div class="header">
+      <div class="nav">
+        <div class="designed">
+          <p>
+            designed by french toast mafia.
+          </p>
+        <nav class="navbar">
+          <a href="index.html">HOME</a> |
+          <a href="cocktails.html">COCKTAILS</a> |
+          <a href="aboutus.html">About Us</a> |
+        </nav>
+    <div id="frm">
+      <form action="process.php" method="POST">
+        <p>
+          <label>Username:</label>
+          <input type="text" id="user" name="user"/>
+        </p>
+        <p>
+          <label>Password</label>
+          <input type="password" id="pass" name="pass"/>
+        </p>
+        <p>
+          <input type="submit" id="btn" value="Login"/>
+        </p>
+    </div>
+  </div>
+</div>
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>
