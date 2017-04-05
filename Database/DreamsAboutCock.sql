11092c3ba15a34ced0990cd738a789eb0e2041b6
KingCarmo <kevincarmody97@gmail.com>
2017-04-05 12:55:42 +0100

    Updated

--- Database/DreamsAboutCock.sql
+++ Database/DreamsAboutCock.sql
@@ -40,32 +40,31 @@ Alcohol VARCHAR(320) NOT NULL
 );
 
 INSERT INTO Beginner (id, Cocktails, Fruit, Alcohol)
-VALUES (1, 'Gimlet', 'Lime', 'Gin');
-VALUES (2, 'Cosmopolitan', 'Cranberry', 'Gin');
-VALUES (3, 'Hemingway Daiquiri', 'Lime''Grapefruit', 'Rum');
-VALUES (4, 'Negroni', 'Orange', 'Gin');
-VALUES (5, 'Sidecar', 'Lemon', 'Brandy');
 
-INSERT INTO Intermediate(id, Cocktails, Fruit, Alcohol)
-VALUES (6,'Blue Coconut', 'Coconut', 'Curacao''Vodka');
-VALUES (7,'Cake by the Ocean', 'Pineapple''Orange', 'Vodka''Rum');
-VALUES (8,'Rainbow Sangria', 'Blackberries''Blueberries''Kiwis''Pinapple''Mango''Straberries''Limes', 'White Wine');
-VALUES (9,'Malibu Sunset', 'Orange''Pineapple''Cherries', 'Vermouth''Rum');
+VALUES (1, 'Gimlet', 'Lime', 'Gin'), 
+(2, 'Cosmopolitan', 'Cranberry', 'Gin'), 
+(3, 'Hemingway Daiquiri', 'Lime''Grapefruit', 'Rum'), 
+(4, 'Negroni', 'Orange', 'Gin'), 
+(5, 'Sidecar', 'Lemon', 'Brandy');
 
-INSERT INTO Expert(id, Cocktails, Fruit, Alcohol)
-VALUES (10,'Mai-Tai', 'Lime', 'Rum');
-VALUES (11,'Old-Fashioned', 'Orange''Cherry', 'Whisky');
-VALUES (12,'Alien Sky','Pineapple' 'Orange', 'Rum');
-VALUES (13,'Chocolate Mintini', 'Coco', 'Vodka');
+INSERT INTO intermediate(id, Cocktails, Fruit, Alcohol)
+VALUES (6,'Blue Coconut', 'Coconut', ' Curacao and ' 'Vodka'),
+ (7,'Cake by the Ocean', 'Pineapple''Orange', ' Vodka and  ' 'Rum '),
+ (8,'Rainbow Sangria', 'Blackberries ' ' Blueberries '' Kiwis '' Pinapple '' Mango ' 'Straberries '' Limes ', 'White Wine'),
+ (9,'Malibu Sunset', 'Orange ' ' Pineapple' ' Cherries ', ' Vermouth and ' 'Rum ');
+
+INSERT INTO expert(id, Cocktails, Fruit, Alcohol)
+VALUES (10,'Mai-Tai', 'Lime', 'Rum'),
+ (11,'Old-Fashioned', 'Orange''Cherry', 'Whisky'),
+ (12,'Alien Sky','Pineapple' 'Orange', 'Rum'),
+ (13,'Chocolate Mintini', 'Coco', 'Vodka');
 
 INSERT INTO Summer(id, Cocktails, Fruit, Alcohol)
-VALUES (14,'Pina Colada', 'Coconut''Pineapple', 'Rum');
-VALUES (15,'Cuba Libre', 'Lime', 'Rum');
-VALUES (16,'Hurricane','Lemon''Lime', 'Rum');
-VALUES (17,'Blackberries', 'Blackberries' 'Mint', 'Vodka''Rum');
+VALUES (14,'Pina Colada', 'Coconut' 'Pineapple', 'Rum'),
+ (15,'Cuba Libre', 'Lime', 'Rum'),
+ (16,'Hurricane','Lemon ' 'Lime', 'Rum'),
+ (17,'Blackberries', 'Blackberries' 'Mint', 'Vodka''Rum');
 
 
-SELECT 
-    *
-FROM
-    Beginner;
+select * FROM intermediate
+	
